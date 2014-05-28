01c342f0eea585d3edf3c9475bd75b816cc28abc
Djsurry <djs@fluffy.home>
2014-05-28 18:16:19 -0400

    I started the game
    I just started how the players  pick their class.

---
+++ Caves and Goblins.py
@@ -0,0 +1,16 @@
+print ('Welcome to Caves and Dragons! Please type your hero\'s name.')
+UserHero = input()
+
+print ("""Your were recruited by the Wizard of the West to eliminate the whole
+goblin population in Morath, your home country. As a test, the wizard has placed
+you in a cave full of traps and unknown dangers. But before you begin, we must
+build your character. Press enter to continue.""")
+
+print ("""Type your race. Your can be a elf, a human, a dwarf, or a troll. Each
+of these races has their limits to specific classes. For example, if you are a
+troll or a dwarf, you can't be a wizard or a cleric as a class.""")
+UserRace = input()
+
+print (""" Now that you have picked your race,""",UserHero,
+"""you need to chose a class. Their are 
+

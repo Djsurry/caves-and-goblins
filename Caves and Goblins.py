1b3d76d528645479bbb739428824fcafe4b45aff
Djsurry <djs@fluffy.home>
2014-05-29 20:59:28 -0400

    Trying to get back to normal

--- Caves and Goblins.py
+++ Caves and Goblins.py
@@ -13,10 +13,10 @@ a class.""")
 UserRace = input()
 
 print ("""Now that you have picked your race,""",UserHero,""",you need to chose
-a class. There are four classes you can chose from. Cleric,
-wizard, warrior, and theif are those four clases. They each have their pros and
-cons. I will go into dephth on those later. There are also resitrictions. Dwarfs
-and trolls can't be clerics or wizards, and a elf cant be a theif. Type your
+a class. There are four classes you can chose from. Cleric, wizard, warrior, and
+theif are those four clases. They each have their pros and cons. I will go into
+dephth on those later. There are also resitrictions. Dwarfs and trolls can't be
+clerics or wizards, and a elf cant be a theif. Type your
 class, please use only lowercase letters.""")
 UserClass = input()
 
@@ -42,32 +42,58 @@ if UserRace == 'elf' and UserClass == 'theif':
 
 if UserRace == 'elf':
     print ("""Congradulations! You are an elf""", UserClass,"""! Now I can tell you
-your powers, which are decided by your class and race.""")
-
+your powers, which are decided by your class and race. Press the spacebar to continue.""")
+input()
+ 
 if UserRace == 'troll' or 'human' or 'dwarf':
     print ("""Congradulations! You are a""",UserRace, UserClass,"""! Now I can tell
-you your powers, which are decided by your class and race.""")
+you your powers, which are decided by your class and race. Press the spacebar to
+continue.""")
+    input()
 
 if UserRace == 'elf':
     print ("""Since you are an elf, you get certain boosts. If you are carrying a bow,
 you have a better chance of hitting your opponent. You also have a better chance of
-getting away or hiding from and enemy that is too powerful for you to beat."""
-
-if UserRace == 'troll' :
+getting away or hiding from and enemy that is too powerful for you to beat. Press the
+spacebar to continue."""
+    input()
+           
+if UserRace == 'troll':
     print ("""Since you are a troll, you get certain boosts. If you are fighting a
 monster where no ranged weapons are used, you have a better chance of winning since you
-have a strengh bounus. You also have extra HP because of your toughness.""")
+have a strengh bounus. You also have extra HP because of your toughness. Press the
+spacebar to continue.""")
+    input()
 
 if UserRace == 'dwarf':
     print ("""Since you are a dwarf, when you are using an ax or a club you deal 2X as
 much damage as if you were a different race. Also, you have the ability to mine in caves
-in search for gold or diamond.""")
-
+in search for gold or diamond. Press the spacebar to continue.""")
+    input()
+           
 if UserRace == 'human':
     print ("""Since you chose human as your race, you have the ability to out-wit your
 enemy. Also, you can presuade people and even monsters to do what you want them to \(for
-instance, you could convince a king to let you and your friends stay in his castle.
-s           
+instance, you could convince a king to let you and your friends stay in his castle.Press
+the spacebar to continue""")
+    input()
+
+if UserClass == 'thief':
+    print ("""The classs you have chosen gives you two advantages. Being a theif, you 
+deal double damage using small weapons. You also have a ability to steal things from
+merchants and any unlucky person \(or beast!\) you might pass.""")
+
+if UserClass == 'cleric':
+    print ("""The class you have selected gives you two advantages. Being a cleric,
+you can heal your ally and other people you might encounter on your journey. Also,
+you have ten extra magic points.""")
+
+if UserClass == 'warrior'
+    print ("""The class you have selected gives you certain ...""")
+
+
+           
+           
            
 
 
